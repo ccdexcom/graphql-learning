212eb820eda624d7ad69cf342fc9d7c283ff3335
Chad <136512293+ccdexcom@users.noreply.github.com>
2024-08-13 13:03:27 -0400

    Add more to the example schema

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
 const { GraphQLHandler } = require("graphql-mocks");
 
+const cities = new Map();
+
 const handler = new GraphQLHandler({
   middlewares: [],
   resolverMap: {
     Query: {
-      helloWorld() {
+      helloWorld(parent) {
         return `hello`;
       },
     },
@@ -18,6 +20,23 @@ const handler = new GraphQLHandler({
         type Query {
           helloWorld: String
           helloMars: String
+          helloNewYork: City
+          cities: [String]
+        }
+
+        type City {
+          name: String
+          population: Int
+          area: Int
+          density: Int
+          country: Country
+        }
+
+        type Country {
+          name: String
+          population: Int
+          area: Int
+          density: Int
         }
       `,
   },
